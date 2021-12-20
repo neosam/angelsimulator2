925377438df9bac4d3acd3e764aec198c88b4fd6
Simon Goller <neosam+github@posteo.de>
2021-12-20 18:05:06 +0100

    Introduce states and system sets

--- engelsimulator2/src/main.rs
+++ engelsimulator2/src/main.rs
@@ -7,33 +7,40 @@ mod entity;
 mod event;
 mod resource;
 mod system;
+mod gamestate;
+
+use gamestate::GameState;
 
 fn main() {
     let mut builder = App::build();
-    builder.add_event::<event::collision_events::SanityDrainEvent>();
     builder.add_plugins(DefaultPlugins);
     builder.add_plugin(ShapePlugin);
     builder.add_plugin(PhysicsPlugin::default());
     #[cfg(target_arch = "wasm32")]
     builder.add_plugin(bevy_webgl2::WebGL2Plugin);
-    builder.add_startup_system(
-        system::startup::ingame_startup_system
+
+    builder.add_event::<event::collision_events::SanityDrainEvent>();
+    builder.add_state(GameState::Ingame);
+    builder.add_system_set(SystemSet::on_enter(GameState::Ingame)
+        .with_system(system::startup::ingame_startup_system
             .system()
-            .chain(system::handle_error_system.system()),
+            .chain(system::handle_error_system.system()))
     );
-    builder.add_system(system::input_system.system());
-    builder.add_system(
-        system::player_controller_system
-            .system()
-            .chain(system::handle_error_system.system()),
-    );
-    builder.add_system(system::camera_movement_system.system());
-    builder.add_system(system::collision_handler_system.system());
-    builder.add_system(system::sanity_drain_system.system());
-    builder.add_system(
-        system::ui_update_system
-            .system()
-            .chain(system::handle_error_system.system()),
+    builder.add_system_set(SystemSet::on_update(GameState::Ingame)
+        .with_system(system::input_system.system())
+        .with_system(system::camera_movement_system.system())
+        .with_system(system::collision_handler_system.system())
+        .with_system(system::sanity_drain_system.system())
+        .with_system(
+            system::ui_update_system
+                .system()
+                .chain(system::handle_error_system.system()),
+        )
+        .with_system(
+            system::player_controller_system
+                .system()
+                .chain(system::handle_error_system.system()),
+        )
     );
 
     builder.run();
